fa0c714a5990695a4bd5dcaa1ca3f4af3e863bf2
Sam Hawkins <shawkins@nutramaxlabs.com>
2025-12-05 14:47:26 -0500

    yes

--- app/components/video-section.tsx
+++ app/components/video-section.tsx
@@ -8,7 +8,20 @@ const VideoSection = () => {
     <div className={classes.container}>
       <Button type="shop" imported_class={classes["shop-primary"]} />
       <div className={classes.video_container}>
-        <Image className={classes.video_img} src={img} alt={""} fill />
+        {/* <Image className={classes.video_img} src={img} alt={""} fill /> */}
+        <iframe
+          src="https://fast.wistia.net/embed/iframe/vb74b8gcu7?web_component=true&seo=true"
+          title="Filaquin Amazon A+ 90ct (4) Video"
+          allow="autoplay; fullscreen"
+          // allowtransparency="true"
+          // frameborder="0"
+          // scrolling="no"
+          // class="wistia_embed"
+          name="wistia_embed"
+          width="100%"
+          height="100%"
+        ></iframe>
+        <script src="https://fast.wistia.net/player.js" async></script>
       </div>
     </div>
   );
